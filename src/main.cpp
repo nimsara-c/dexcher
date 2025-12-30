fffafe899995a288beb249dfee608e3b91aa1c4e
Nimsara Chamindu <nimsarachamidhu@gmail.com>
2025-12-30 20:37:08 +0530

    Remove unused/commented code snippets

--- src/main.cpp
+++ src/main.cpp
@@ -6,30 +6,10 @@
 
 #include "Application.hpp"
 
-/*
-#include <tray/tray.hpp>
-using Tray::Button;
-using Tray::Tray;
-
-void runTrayIcon()
-{
-    Tray::Tray tray("My Tray", "favicon.ico");
-    tray.addEntry(Button("Exit", [&]
-                         { tray.exit(); }));
-
-    tray.run();
-}
-*/
-
 int main()
 {
-
-    // std::thread trayThread(runTrayIcon);
-
     Application app;
     app.run();
-
-    // trayThread.join();
 
     return 0;
 }
