306e1b2c2d9cedfe1a3d59696f5b386dbe4e24b2
nimsara-c <nimsarachamidhu@gmail.com>
2026-01-02 15:37:52 +0530

    Add -mwindows flag to build script

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 windres resources.rc -o resources.o
-g++ -Iinclude -Iinclude/tray resources.o src/main.cpp -pthread -Llib -ltray -o dexcher
+g++ -Iinclude -Iinclude/tray resources.o src/main.cpp -mwindows -pthread -Llib -ltray -o dexcher
